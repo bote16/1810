5619c1f4921a798ee49073f5e663193d8538ee76
bote16 <jbotejara@gmail.com>
2022-10-18 20:13:34 -0300

    2

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var form = document.getElementById("formulario");
 form.addEventListener(
   "submit",
   function (event) {
+    event.preventDefault();
     form.classList.add("was-validated");
     if (!form.checkValidity()) {
-      event.preventDefault();
       return;
     }
 
